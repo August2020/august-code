190567ec8419a8ed9dd8ea7c98558a9615bdd68f
Szymon Augustyn <szymona2008@gmail.com>
2024-04-26 03:42:02 +0200

    Articles model update

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'content',
+        'image',
+        'created_at',
+        'modified_at',
+        'active',
+        'deleted',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(ArticleCategory::class, 'article_category_id');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(ArticleTag::class);
+    }
 }
