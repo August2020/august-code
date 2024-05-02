9649d142d3a5af78e730df80d60f43abf72b896e
Szymon Augustyn <szymona2008@gmail.com>
2024-05-02 18:05:18 +0200

    Update

--- resources/js/Components/Navigation.jsx
+++ resources/js/Components/Navigation.jsx
@@ -4,92 +4,96 @@ import { Link } from "@inertiajs/react";
 
 const Navigation = () => {
     return (
-        <header className="bg-white border-b border-gray-200">
-            <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-                <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-                    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                        <Link
-                            href="https://flowbite.com/"
-                            className="flex items-center space-x-3 rtl:space-x-reverse"
+        <nav class="bg-white dark:bg-gray-900 px-4 fixed w-full z-20 top-0 start-0">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-4">
+                <Link
+                    href="#"
+                    className="flex items-center space-x-3 rtl:space-x-reverse"
+                >
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                        August Code
+                    </span>
+                </Link>
+                <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    <button
+                        type="button"
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        Zadzwoń
+                    </button>
+                    <button
+                        data-collapse-toggle="navbar-sticky"
+                        type="button"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none"
+                        aria-controls="navbar-sticky"
+                        aria-expanded="false"
+                    >
+                        <span className="sr-only">Toggle menu</span>
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
                         >
-                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                                August Code
-                            </span>
-                        </Link>
-                        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                            <button
-                                type="button"
-                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h16m-7 6h7"
+                            />
+                        </svg>
+                    </button>
+                </div>
+                <div
+                    className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                    id="navbar-sticky"
+                >
+                    <ul className="flex flex-col h-screen md:h-auto md:sticky md:top-0 p-4 md:p-0 mt-4 font-medium md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0">
+                        <li>
+                            <Link
+                                href="#"
+                                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                                aria-current="page"
                             >
-                                Zadzwoń
-                            </button>
-                            <button
-                                data-collapse-toggle="navbar-sticky"
-                                type="button"
-                                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none"
-                                aria-controls="navbar-sticky"
-                                aria-expanded="false"
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="#"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                             >
-                                <span className="sr-only">Toggle menu</span>
-                                <svg
-                                    className="w-6 h-6"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M4 6h16M4 12h16m-7 6h7"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
-                        <div
-                            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-                            id="navbar-sticky"
-                        >
-                            <ul className="flex flex-col h-screen md:h-auto md:sticky md:top-0 p-4 md:p-0 mt-4 font-medium md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0">
-                                <li>
-                                    <Link
-                                        href="#"
-                                        className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                                        aria-current="page"
-                                    >
-                                        Home
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        href="#"
-                                        className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                                    >
-                                        About
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        href="#"
-                                        className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                                    >
-                                        Services
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        href="#"
-                                        className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                                    >
-                                        Contact
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="#"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >
+                                Services
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="#"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >
+                                Blog
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="#"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </header>
+        </nav>
     );
 };
 
--- resources/js/Layouts/DefaultLayout.jsx
+++ resources/js/Layouts/DefaultLayout.jsx
@@ -3,10 +3,10 @@ import { initFlowbite } from "flowbite"; // flowbite init
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen bg-gray-100">
+        <div className="min-h-screen bg-white-100">
             <Navigation></Navigation>
             <main className="mt-10 sm:mt-20 pb-10 sm:pb-20">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="max-w-7xl px-4 mx-auto">
                     <div className="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                         {children}
                     </div>
